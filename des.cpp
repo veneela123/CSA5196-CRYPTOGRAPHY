9e43585c9fb0020df07f4ce22c6e8b0cb41ad6f5
veneela123 <113539505+veneela123@users.noreply.github.com>
2023-11-23 11:02:37 +0530

    Update des.cpp

--- des.cpp
+++ des.cpp
@@ -1,54 +1,63 @@
 #include <stdio.h>
 #include <stdint.h>
-
-// Initial Permutation Table (IP)
-const int initialPermutationTable[64] = { 58, 50, 42, 34, 26, 18, 10, 2,
-                                          60, 52, 44, 36, 28, 20, 12, 4,
-                                          62, 54, 46, 38, 30, 22, 14, 6,
-                                          64, 56, 48, 40, 32, 24, 16, 8,
-                                          57, 49, 41, 33, 25, 17, 9, 1,
-                                          59, 51, 43, 35, 27, 19, 11, 3,
-                                          61, 53, 45, 37, 29, 21, 13, 5,
-                                          63, 55, 47, 39, 31, 23, 15, 7 };
-
-// Function to perform an initial permutation on the data
-void initialPermutation(uint64_t *data) {
-    // Implement initial permutation logic here
-}
-
-// Function to perform a key schedule iteration for decryption
-void keyScheduleIteration(uint64_t *key, int round) {
-    // Implement key schedule iteration logic here
-    // This function should modify the key based on the round number
-}
-
-// Function to generate decryption keys
-void generateDecryptionKeys(uint64_t *originalKey, uint64_t *decryptionKeys) {
-    int round;
-
-    // Perform initial permutation on the original key
-    initialPermutation(originalKey);
-
-    // Generate the 16 decryption keys in reverse order
-    for (round = 15; round >= 0; round--) {
-        keyScheduleIteration(originalKey, round);
-        decryptionKeys[15 - round] = *originalKey;
+void reverseKeyGeneration(uint64_t* keys) 
+{
+    uint64_t temp;
+    for (int i = 0; i < 8; i++) 
+	{
+        temp = keys[i];
+        keys[i] = keys[15 - i];
+        keys[15 - i] = temp;
     }
 }
-
-int main() {
-    uint64_t originalKey = 0x133457799BBCDFF1; // Example 64-bit key
-    uint64_t decryptionKeys[16];
-
-    // Generate decryption keys
-    generateDecryptionKeys(&originalKey, decryptionKeys);
-
-    // Display the generated decryption keys
-    printf("Decryption Keys:\n");
-    for (int i = 0; i < 16; i++) {
-        printf("K%d: %llx\n", i + 1, decryptionKeys[i]);
+uint64_t desDecrypt(uint64_t ciphertext, uint64_t* keys)
+{  
+    uint64_t permutation = 0;
+    for (int i = 0; i < 64; i++)
+	{
+        permutation |= ((ciphertext >> (63 - i)) & 0x01) << (63 - i);
     }
-
+    for (int round = 0; round < 16; round++) 
+	{
+        uint64_t expansion = 0;
+        for (int i = 0; i < 48; i++) 
+		{
+            expansion |= ((permutation >> (31 - i)) & 0x01) << (47 - i);
+        }
+        uint64_t roundKey = keys[round];
+        uint64_t roundOutput = expansion ^ roundKey;
+        permutation = roundOutput;
+    }
+    uint64_t plaintext = 0;
+    for (int i = 0; i < 64; i++) 
+	{
+        plaintext |= ((permutation >> (63 - i)) & 0x01) << (63 - i);
+    }
+    return plaintext;
+}
+int main() 
+{
+    uint64_t ciphertext = 0x0123456789ABCDEF;
+    uint64_t keys[16] = {
+        0x0000000000000000,
+        0x1111111111111111,
+        0x2222222222222222,
+        0x3333333333333333,
+        0x4444444444444444,
+        0x5555555555555555,
+        0x6666666666666666,
+        0x7777777777777777,
+        0x8888888888888888,
+        0x9999999999999999,
+        0xAAAAAAAAAAAAAAAA,
+        0xBBBBBBBBBBBBBBBB,
+        0xCCCCCCCCCCCCCCCC,
+        0xDDDDDDDDDDDDDDDD,
+        0xEEEEEEEEEEEEEEEE,
+        0xFFFFFFFFFFFFFFFF
+    };
+    reverseKeyGeneration(keys);
+    uint64_t plaintext = desDecrypt(ciphertext, keys);
+    printf("Decrypted plaintext: %016llx\n", plaintext);
     return 0;
 }
-
