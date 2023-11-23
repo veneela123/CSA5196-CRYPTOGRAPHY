bb4c4f56a9097b14b9c38e4ecf48d59b2d2e363d
veneela123 <113539505+veneela123@users.noreply.github.com>
2023-11-23 10:59:29 +0530

    Update poly_alphabetickey.cpp

--- poly_alphabetickey.cpp
+++ poly_alphabetickey.cpp
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
-
-void encrypt(char *message, char *key) {
+void encrypt(char *message, char *key) 
+{
     int keyLen = strlen(key);
     int messageLen = strlen(message);
 
-    for (int i = 0; i < messageLen; i++) {
-        if (isalpha(message[i])) {
+    for (int i = 0; i < messageLen; i++) 
+	{
+        if (isalpha(message[i])) 
+		{
             char base = isupper(message[i]) ? 'A' : 'a';
-            int shift = tolower(key[i % keyLen]) - 'a';
-
-            // Apply monoalphabetic substitution
+            int shift = tolower(key[i % keyLen]) - 'a';      
             message[i] = (char)(((tolower(message[i]) - base + shift) % 26) + base);
         }
     }
 }
-
-int main() {
+int main() 
+{
     char message[100];
     char key[100];
 
@@ -27,20 +27,16 @@ int main() {
     printf("Enter the key: ");
     fgets(key, sizeof(key), stdin);
 
-    // Ensure the key and message are of the same length
+ 
     int keyLen = strlen(key);
     int messageLen = strlen(message);
 
-    if (keyLen > messageLen) {
+    if (keyLen > messageLen) 
+	{
         printf("Error: Key length is longer than the message length.\n");
         return 1;
     }
-
-    // Encrypt the message using polyalphabetic substitution
     encrypt(message, key);
-
     printf("Encrypted message: %s", message);
-
     return 0;
 }
-
