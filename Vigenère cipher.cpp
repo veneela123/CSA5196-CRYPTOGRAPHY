164cf55c86cc725babaa2d47e9150df71cefe746
veneela123 <113539505+veneela123@users.noreply.github.com>
2023-11-23 11:00:02 +0530

    Update Vigenère cipher.cpp

--- Vigenère cipher.cpp
+++ Vigenère cipher.cpp
@@ -1,60 +1,43 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+void vigenereCipher(char* key, char* message) 
+{
+    int keyLen = strlen(key);
+    int msgLen = strlen(message);
+    char encryptedMsg[msgLen];
 
-// Function to encrypt the plaintext using the one-time pad Vigenère cipher
-void encrypt(char plaintext[], int key[], int n) {
-    for (int i = 0; i < n; i++) {
-        if (plaintext[i] != ' ') {
-            // Encrypt only alphabetical characters
-            plaintext[i] = (plaintext[i] - 'a' + key[i]) % 26 + 'a';
+    for (int i = 0, j = 0; i < msgLen; i++) 
+	{
+        char c = message[i];
+
+        if (isalpha(c)) 
+		{
+            char base = isupper(c) ? 'A' : 'a';
+            int ki = tolower(key[j % keyLen]) - 'a';
+            int mi = tolower(c) - base;
+            int ci = (mi + ki) % 26;
+            encryptedMsg[i] = ci + base;
+            j++;
+        } else {
+            encryptedMsg[i] = c;
         }
     }
+    encryptedMsg[msgLen] = '\0';
+    printf("Encrypted message: %s\n", encryptedMsg);
 }
+int main() 
+{
+    char key[100];
+    char message[100];
 
-// Function to decrypt the ciphertext using the one-time pad Vigenère cipher
-void decrypt(char ciphertext[], int key[], int n) {
-    for (int i = 0; i < n; i++) {
-        if (ciphertext[i] != ' ') {
-            // Decrypt only alphabetical characters
-            ciphertext[i] = (ciphertext[i] - 'a' - key[i] + 26) % 26 + 'a';
-        }
-    }
-}
+    printf("Enter the key: ");
+    scanf("%s", key);
 
-int main() {
-    char plaintext[100];
-    int key[100];
-    int n;
+    printf("Enter the message: ");
+    scanf(" %[^\n]s", message);
 
-    // Get user input for plaintext
-    printf("Enter the plaintext: ");
-    fgets(plaintext, sizeof(plaintext), stdin);
-    // Remove newline character from the input
-    plaintext[strcspn(plaintext, "\n")] = '\0';
-
-    n = strlen(plaintext);
-
-    // Get user input for key
-    printf("Enter the key (space-separated integers): ");
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &key[i]);
-    }
-
-    // Encrypt the plaintext
-    encrypt(plaintext, key, n);
-
-    // Print the encrypted ciphertext
-    printf("Encrypted Ciphertext: %s\n", plaintext);
-
-    // Decrypt the ciphertext to find the key
-    char ciphertext[100];
-    strcpy(ciphertext, plaintext); // Save the ciphertext before decryption
-
-    decrypt(ciphertext, key, n);
-
-    // Print the decrypted plaintext
-    printf("Decrypted Plaintext: %s\n", ciphertext);
+    vigenereCipher(key, message);
 
     return 0;
 }
-
