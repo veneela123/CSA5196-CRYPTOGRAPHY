13d1d2e9e854594e8eb126e4a32badea0dc59d82
veneela123 <113539505+veneela123@users.noreply.github.com>
2023-11-23 11:03:08 +0530

    Update ecb.cpp

--- ecb.cpp
+++ ecb.cpp
@@ -1,57 +1,49 @@
 #include <stdio.h>
 #include <string.h>
-
-// Function to perform ECB encryption
-void ecb_encrypt(const char* plaintext, const char* key, char* ciphertext, int len) {
-    int block_size = 8; // Assuming a block size of 8 bytes
-
-    for (int i = 0; i < len; i += block_size) {
-        // Encrypt each block independently
-        for (int j = 0; j < block_size; j++) {
+void ecb_encrypt(const char* plaintext, const char* key, char* ciphertext, int len) 
+{
+    int block_size = 8; 
+    for (int i = 0; i < len; i += block_size) 
+	{
+        for (int j = 0; j < block_size; j++) 
+		{
             ciphertext[i + j] = plaintext[i + j] ^ key[j];
         }
     }
 }
-
-// Function to perform ECB decryption
-void ecb_decrypt(const char* ciphertext, const char* key, char* plaintext, int len) {
-    // Decryption is the same as encryption in ECB mode
+void ecb_decrypt(const char* ciphertext, const char* key, char* plaintext, int len) 
+{
     ecb_encrypt(ciphertext, key, plaintext, len);
 }
-
-int main() {
-    // Get user input for plaintext
+int main() 
+{
     printf("Enter the plaintext: ");
-    char plaintext[100]; // Assuming a maximum length of 100 characters
+    char plaintext[100]; 
     fgets(plaintext, sizeof(plaintext), stdin);
-    plaintext[strcspn(plaintext, "\n")] = '\0'; // Remove the newline character
+    plaintext[strcspn(plaintext, "\n")] = '\0'; 
 
-    // Get user input for key
     printf("Enter the key: ");
-    char key[100]; // Assuming a maximum length of 100 characters
+    char key[100]; 
     fgets(key, sizeof(key), stdin);
-    key[strcspn(key, "\n")] = '\0'; // Remove the newline character
+    key[strcspn(key, "\n")] = '\0'; 
 
     int len = strlen(plaintext);
     char ciphertext[len];
 
-    // Encrypt the plaintext
     ecb_encrypt(plaintext, key, ciphertext, len);
 
-    // Print the ciphertext
+    
     printf("Ciphertext: ");
-    for (int i = 0; i < len; i++) {
+    for (int i = 0; i < len; i++) 
+	{
         printf("%02x ", (unsigned char)ciphertext[i]);
     }
     printf("\n");
 
-    // Decrypt the ciphertext
     char decrypted_text[len];
     ecb_decrypt(ciphertext, key, decrypted_text, len);
 
-    // Print the decrypted plaintext
     printf("Decrypted Text: %s\n", decrypted_text);
 
     return 0;
 }
-
